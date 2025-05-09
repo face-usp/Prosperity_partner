3937e2ff3ea51c3dc7c4d3242e072852e848e428
vista-treat <91408295+pritesh177@users.noreply.github.com>
2025-05-10 01:59:31 +0530

    fix: responsiveness

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import BackGround1 from "../../assets/BackGround1.jpg";
 const Home = () => {
   return (
     <div className="pt-[80px]">
-      <div className="relative overflow-hidden p-8 sm:p-6 md:p-8 w-full  h-[670px]">
+      <div className="relative overflow-hidden p-5 sm:p-6 md:p-8 w-full  h-[670px]">
         <img
           src={BackGround1}
           alt="Background"
@@ -12,7 +12,7 @@ const Home = () => {
         />
         <div className="absolute bottom-50 left-25">
           <div className="flex flex-col items-center justify-center text-center space-y-4">
-            <h2 className="text-3xl sm:text-4xl md:text-5xl font-serif text-gray-900 leading-tight max-w-3xl">
+            <h2 className="text-3xl sm:text-3xl md:text-5xl font-serif text-gray-900 leading-tight max-w-3xl text-left sm:text-left">
               Crafting Wealth,
               <br />
               Backed by Wisdom
